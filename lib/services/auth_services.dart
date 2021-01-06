dc6cec186aaeb5c30cad20db7077e7c4a7b00393
SasqiaAyu <sasqia.ayul@gmail.com>
2021-01-07 04:31:53 +0700

    firebase

---
+++ lib/services/auth_services.dart
@@ -0,0 +1,20 @@
+part of 'services.dart';
+
+class AuthServices {
+  static FirebaseAuth _auth = FirebaseAuth.instance;
+
+  static Future signUp(String email, String password, String name,
+      List<String> selectedGenres, String selectdLanguage) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+
+      User user = result.user.convertToUser(
+          name: name,
+          selectedGenres: selectedGenres,
+          selectedLanguage: selectdLanguage);
+
+          
+    } catch (e) {}
+  }
+}
