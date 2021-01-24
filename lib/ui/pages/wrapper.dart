1f097f2085ddd383b74c68347c6578f36ede3014
SasqiaAyu <sasqia.ayul@gmail.com>
2021-01-24 15:52:07 +0700

    update

--- lib/ui/pages/wrapper.dart
+++ lib/ui/pages/wrapper.dart
@@ -12,13 +12,15 @@ class Wrapper extends StatelessWidget {
       }
     } else {
       if (!(prevPageEvent is GoToMainPage)) {
+
+        context.bloc<UserBloc>().add(LoadUser(firebaseUser.uid));        
         prevPageEvent = GoToMainPage();
         context.bloc<PageBloc>().add(prevPageEvent);
       }
     }
     return BlocBuilder<PageBloc, PageState>(
-        builder: (_, PageState) => (PageState is OnSplashPage)
+        builder: (_, pageState) => (pageState is OnSplashPage)
                 ? SplashPage()
-                : (PageState is OnLoginPage) ? SignInpage() : MainPage());
+                : (pageState is OnLoginPage) ? SignInpage() : MainPage());
   }
 }
