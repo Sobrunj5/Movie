8e96a96fd9f22c99bb064fe87f536b4c082e0fc5
SasqiaAyu <sasqia.ayul@gmail.com>
2021-01-25 14:33:40 +0700

    sign in

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -25,7 +25,47 @@ class _SignInpageState extends State<SignInpage> {
                   style: blackTextFont.copyWith(fontSize: 20),
                 ),
               ),
-              
+              TextField(),
+              SizedBox(
+                height: 16,
+              ),
+              TextField(),
+              SizedBox(
+                height: 6,
+              ),
+              Row(
+                children: <Widget>[
+                  Text("Forgot Password",
+                      style: greyTextFont.copyWith(
+                          fontSize: 12, fontWeight: FontWeight.w400)),
+                  Text(
+                    "Get Now",
+                    style: purpleTextFont.copyWith(fontSize: 12),
+                  ),
+                ],
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                margin: EdgeInsets.only(top: 40, bottom: 30),
+                child: FloatingActionButton(
+                  child: Icon(Icons.arrow_forward),
+                  backgroundColor: mainColor,
+                  onPressed: () {},
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Start Fresh Now?",
+                    style: greyTextFont.copyWith(fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    "Sign Up",
+                    style: purpleTextFont,
+                  )
+                ],
+              )
             ],
           )),
     );
