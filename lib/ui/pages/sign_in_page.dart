01a2ed922ed659a94c4a240f16c96b53c4cec6b5
SasqiaAyu <sasqia.ayul@gmail.com>
2021-01-25 14:55:31 +0700

    sign in

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -6,66 +6,99 @@ class SignInpage extends StatefulWidget {
 }
 
 class _SignInpageState extends State<SignInpage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isEmailValid = false;
+  bool isPasswordValid = false;
+  bool isSigningIn = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
           padding: EdgeInsets.symmetric(horizontal: defaulMargin),
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 70,
-                child: Image.asset("assets/logo.png"),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 70, bottom: 40),
-                child: Text(
-                  "Welcom Bck,\nExplore!",
-                  style: blackTextFont.copyWith(fontSize: 20),
-                ),
-              ),
-              TextField(),
-              SizedBox(
-                height: 16,
-              ),
-              TextField(),
-              SizedBox(
-                height: 6,
-              ),
-              Row(
+          child: ListView(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text("Forgot Password",
-                      style: greyTextFont.copyWith(
-                          fontSize: 12, fontWeight: FontWeight.w400)),
-                  Text(
-                    "Get Now",
-                    style: purpleTextFont.copyWith(fontSize: 12),
+                  SizedBox(height: 40,),
+                  SizedBox(
+                    height: 70,
+                    child: Image.asset("assets/logo.png"),
                   ),
-                ],
-              ),
-              Container(
-                width: 50,
-                height: 50,
-                margin: EdgeInsets.only(top: 40, bottom: 30),
-                child: FloatingActionButton(
-                  child: Icon(Icons.arrow_forward),
-                  backgroundColor: mainColor,
-                  onPressed: () {},
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  Text(
-                    "Start Fresh Now?",
-                    style: greyTextFont.copyWith(fontWeight: FontWeight.w400),
+                  Container(
+                    margin: EdgeInsets.only(top: 70, bottom: 40),
+                    child: Text(
+                      "Welcom Back,\nExplore!",
+                      style: blackTextFont.copyWith(fontSize: 20),
+                    ),
                   ),
-                  Text(
-                    "Sign Up",
-                    style: purpleTextFont,
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      labelText: "Email Address",
+                      hintText: "Email Address",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                            labelText: "Password",
+                            hintText: "Password",
+                            ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text("Forgot Password",
+                          style: greyTextFont.copyWith(
+                              fontSize: 12, fontWeight: FontWeight.w400)),
+                      Text(
+                        "Get Now",
+                        style: purpleTextFont.copyWith(fontSize: 12),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      margin: EdgeInsets.only(top: 40, bottom: 30),
+                      child: FloatingActionButton(
+                        child: Icon(Icons.arrow_forward),
+                        backgroundColor: mainColor,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        "Start Fresh Now?",
+                        style: greyTextFont.copyWith(fontWeight: FontWeight.w400),
+                      ),
+                      Text(
+                        "Sign Up",
+                        style: purpleTextFont,
+                      )
+                    ],
                   )
                 ],
-              )
+              ),
             ],
           )),
     );
