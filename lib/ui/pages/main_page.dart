b6a0839bea2ad3e517d7a68322d0e689f7375b56
SasqiaAyu <sasqia.ayul@gmail.com>
2021-01-29 08:51:58 +0700

    update

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -9,23 +9,42 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Main Page")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BlocBuilder<UserBloc, UserState>(
-                builder: (_, userState) => (userState is UserLoaded)
-                    ? Text(userState.user.name)
-                    : SizedBox()),
-            RaisedButton(
-                child: Text("Sign Out"),
-                onPressed: () {
-                  AuthServices.signOut();
-                }),
-          ],
-        ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            color: accentColor1,
+          ),
+          SafeArea(
+              child: Container(
+            color: Color(0xFFF6F7F9),
+          )),
+          ListView(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                height: 70,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
+                )),
+          )
+        ],
       ),
     );
   }
+}
+
+class BottomNavBarClipper extends CustomClipper <Path> {
+  @override
+  Path getClip(Size size) {
+      throw UnimplementedError();
+    }
+  
+    @override
+    bool shouldReclip(CustomClipper<Path> oldClipper) {
+    throw UnimplementedError();
+  }
+  
 }
