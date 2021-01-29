f6d28cebd6d38be18967f97a24450817c0f16cc4
SasqiaAyu <sasqia.ayul@gmail.com>
2021-01-29 09:24:31 +0700

    NavBar

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -19,32 +19,59 @@ class _MainPageState extends State<MainPage> {
             color: Color(0xFFF6F7F9),
           )),
           ListView(),
+          createCustomBottomNavBar(),
           Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-                height: 70,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20)),
-                )),
-          )
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 46,
+                width: 46,
+                margin: EdgeInsets.only(bottom: 42),
+                child: FloatingActionButton(
+                  elevation: 0, backgroundColor: accentColor2,
+                    child: SizedBox(
+                      height: 26,
+                      width: 26,
+                      child: Icon (MdiIcons.walletPlus, color: Colors.black.withOpacity(0.54),),
+                    ),
+                    onPressed: () {}),
+              ))
         ],
       ),
     );
   }
+
+  Widget createCustomBottomNavBar() => Align(
+        alignment: Alignment.bottomCenter,
+        child: ClipPath(
+          clipper: BottomNavBarClipper(),
+          child: Container(
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+              )),
+        ),
+      );
 }
 
-class BottomNavBarClipper extends CustomClipper <Path> {
+class BottomNavBarClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-      throw UnimplementedError();
-    }
-  
-    @override
-    bool shouldReclip(CustomClipper<Path> oldClipper) {
-    throw UnimplementedError();
+    Path path = Path();
+
+    path.lineTo(size.width / 2 - 28, 0);
+    path.quadraticBezierTo(size.width / 2 - 28, 33, size.width / 2, 33);
+    path.quadraticBezierTo(size.width / 2 + 28, 33, size.width / 2 + 28, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    return path;
   }
-  
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
