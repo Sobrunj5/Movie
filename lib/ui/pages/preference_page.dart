1ea37b6d0617c79bb8b2d8a0e22579fe8b9849ac
SasqiaAyu <sasqia.ayul@gmail.com>
2021-02-07 11:52:37 +0700

    pagebloc

--- lib/ui/pages/preference_page.dart
+++ lib/ui/pages/preference_page.dart
@@ -1,8 +1,8 @@
 part of 'pages.dart';
 
 class PreferencePage extends StatefulWidget {
-
   final RegistrationData registrationData;
+
   PreferencePage(this.registrationData);
 
   @override
@@ -13,11 +13,38 @@ class _PreferencePageState extends State<PreferencePage> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async{},
-          child: Scaffold(
+      onWillPop: () async {
+        widget.registrationData.password = "";
+
+        context
+            .bloc<PageBloc>()
+            .add(GoToRegistrationPage(widget.registrationData));
+      },
+      child: Scaffold(
         body: Container(
           color: Colors.white,
           padding: EdgeInsets.symmetric(horizontal: defaulMargin),
+          child: ListView(
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 56,
+                    margin: EdgeInsets.only(top: 20, bottom: 4),
+                    child: GestureDetector(
+                        onTap: () {
+                          widget.registrationData.password = "";
+
+                          context.bloc<PageBloc>().add(
+                              GoToRegistrationPage(widget.registrationData));
+                        },
+                        child: Icon(Icons.arrow_back)),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
