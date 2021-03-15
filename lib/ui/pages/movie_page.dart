7eaad630099006aa25c74843be4e71de2572cb26
SasqiaAyu <sasqia.ayul@gmail.com>
2021-03-15 14:50:17 +0700

    browse movie

--- lib/ui/pages/movie_page.dart
+++ lib/ui/pages/movie_page.dart
@@ -5,7 +5,7 @@ class MoviePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        //note : Header
+        //note : HEADER
         Container(
           decoration: BoxDecoration(
               color: accentColor1,
@@ -90,6 +90,7 @@ class MoviePage extends StatelessWidget {
             }
           }),
         ),
+        //note : NOW PLAYING
         Container(
           margin: EdgeInsets.fromLTRB(defaulMargin, 30, defaulMargin, 12),
           child: Text(
@@ -124,7 +125,72 @@ class MoviePage extends StatelessWidget {
               }
             },
           ),
-        )
+        ),
+        // note : BROWSE MOVIE
+        Container(
+          margin: EdgeInsets.fromLTRB(defaulMargin, 30, defaulMargin, 12),
+          child: Text(
+            "Browse Movie",
+            style: blackTextFont.copyWith(
+                fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        BlocBuilder<UserBloc, UserState>(builder: (_, userState) {
+          if (userState is UserLoaded) {
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: defaulMargin),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: List.generate(
+                    userState.user.selectedGenres.length,
+                    (index) =>
+                        BrowseButton(userState.user.selectedGenres[index])),
+              ),
+            );
+          } else {
+            return SpinKitCircle(
+              color: mainColor,
+              size: 50,
+            );
+          }
+        },
+        ),
+        // note: COMING SOON 
+        Container(
+          margin: EdgeInsets.fromLTRB(defaulMargin, 30, defaulMargin, 12),
+          child: Text(
+            "Coming Soon",
+            style: blackTextFont.copyWith(
+                fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 140,
+          child: BlocBuilder<MovieBloc, MovieState>(
+            builder: (_, movieState) {
+              if (movieState is MovieLoaded) {
+                List<Movie> movies = movieState.movies.sublist(10);
+
+                return ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: movies.length,
+                    itemBuilder: (_, index) => Container(
+                          margin: EdgeInsets.only(
+                              left: (index == 0) ? defaulMargin : 0,
+                              right: (index == movies.length - 1)
+                                  ? defaulMargin
+                                  : 16),
+                          child: ComingSoonCard(movies[index]),
+                        ));
+              } else {
+                return SpinKitFadingCircle(
+                  color: mainColor,
+                  size: 50,
+                );
+              }
+            },
+          ),
+        ),
       ],
     );
   }
--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -6,4 +6,6 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 part 'selectable_box.dart';
 part 'movie_card.dart';
 part 'rating_stars.dart';
+part 'browse_botton.dart';
+part 'coming_soon_card.dart';
 
